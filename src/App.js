fea2977cf3c8e5ea1f4fb45dd3bc1bfc54b66ad4
Jack Wong <wchunwai970225@gmail.com>
2024-11-27 14:27:46 +0800

    Setup project foundation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div></div>
+    <>
+      <Header />
+      <nav></nav>
+      <main></main>
+      <footer></footer>
+    </>
   );
 }
 
